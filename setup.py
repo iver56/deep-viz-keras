1bd6f947e26ea316085f5b01c427306f1f1bc368
iver56 <ij@aiascience.com>
2018-03-08 13:35:13 +0100

    Use setuptools instead of distutils. Require either tensorflow or tensorflow-gpu.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='KerasDeepViz',
     version='0.1',
-    packages=['keras_deep_viz',],
+    packages=['keras_deep_viz', ],
     license='MIT',
     long_description=open('README.md').read(),
     install_requires=[
@@ -11,6 +11,9 @@ setup(
         'Keras',
         'Pillow',
         'matplotlib',
-        'tensorflow',
-    ]
+    ],
+    extras_require={
+        "tf": ["tensorflow>=1.0.0"],
+        "tf_gpu": ["tensorflow-gpu>=1.0.0"],
+    }
 )
